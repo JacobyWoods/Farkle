93268efcc5944513bbd5245e3d269b5bef19952b
JacobyWoods <jcoby6@gmail.com>
2022-12-19 18:55:26 -0700

    Somewhat working version.

--- FarkleGame.py
+++ FarkleGame.py
@@ -1,6 +1,7 @@
 import random
 from collections import Counter
 
+
 class Round:
 
     def __init__(self):
@@ -26,44 +27,20 @@ class Roll:
         self.roll_dice = []
         self.max_dice = 6
         self.roll_dice = [random.randint(1, 6) for x in range(number_dice_to_roll)]
+        self.max_roll_score = check_for_score(self.roll_dice)
 
 
-def create_scoring_combinations_dict():
+def check_for_straight(dice_combo_to_check):
 
-    scoring_combinations = {1: 100,
-                            5: 50,
-                            (1, 1, 1): 300,
-                            (2, 2, 2): 200,
-                            (3, 3, 3): 300,
-                            (4, 4, 4): 400,
-                            (5, 5, 5): 500,
-                            (6, 6, 6): 600,
-                            (1, 2, 3, 4, 5, 6): 1500}
-    for i in range(4, 7):
-        scoring_combinations[(i, i, i, i)] = 1000
-        scoring_combinations[(i, i, i, i, i)] = 2000
-        scoring_combinations[(i, i, i, i, i, i)] = 3000
-
-    for i in range(1, 7):
-        for j in range(i + 1, 7):
-            scoring_combinations[(i, i, i, j, j, j)] = 2500
-
-    for i in range(1, 7):
-        for j in range(i + 1, 7):
-            for k in range(j + 1, 7):
-                scoring_combinations[(i, i, j, j, k, k)] = 1500
-
-    for i in range(1, 7):
-        for j in range(1, 7):
-            if i != j:
-                scoring_combinations[(i, i, i, i, j, j)] = 1500
-
-    return scoring_combinations
+    if sorted(list(dice_combo_to_check)) == [1, 2, 3, 4, 5, 6]:
+        return True
 
 
-def check_for_score(kept_dice_to_check=(1, 3, 3, 1, 3, 1)):
+def print_round_status(round_object):
+    pass
 
-    scoring_combinations = create_scoring_combinations_dict()
+
+def check_for_score(kept_dice_to_check=(3, 3, 1, 5, 1, 5)):
 
     dice_to_check_counter = Counter(kept_dice_to_check)
 
@@ -71,71 +48,114 @@ def check_for_score(kept_dice_to_check=(1, 3, 3, 1, 3, 1)):
     for die_number in range(6):
         dice_count = dice_to_check_counter[die_number]
         if die_number == 1:
-            if sorted(kept_dice_to_check) == [1, 2, 3, 4, 5, 6]:
-                roll_score += 1500
-            elif dice_to_check_counter[die_number] < 4:
-                if dice_to_check_counter[die_number] == 2:
+            if dice_count < 4:
+                if dice_count == 2:
                     pair_count += 1
-                elif dice_to_check_counter[die_number] == 3:
+                elif dice_count == 3:
                     triple_count += 1
-                roll_score += 100 * dice_to_check_counter[die_number]
-            elif dice_to_check_counter[die_number] >= 4:
-                roll_score += 1000 * (dice_to_check_counter[die_number] - 3)
-                if dice_to_check_counter[die_number] == 4:
+                roll_score += 100 * dice_count
+            elif dice_count >= 4:
+                roll_score += 1000 * (dice_count - 3)
+                if dice_count == 4:
                     quad_count += 1
-        elif die_number == 5 and dice_to_check_counter[die_number] <= 2:
-            roll_score += dice_to_check_counter[die_number] * 50
-            if dice_to_check_counter[die_number] == 2:
+        elif die_number == 5 and dice_count <= 2:
+            roll_score += dice_count * 50
+            if dice_count == 2:
                 pair_count += 1
         else:
-            if dice_to_check_counter[die_number] == 2:
+            if dice_count == 2:
                 pair_count += 1
-            elif dice_to_check_counter[die_number] == 3:
+            elif dice_count == 3:
                 roll_score += die_number * 100
                 triple_count += 1
-            elif dice_to_check_counter[die_number] > 3:
-                roll_score += 1000 * (dice_to_check_counter[die_number] - 3)
+            elif dice_count > 3:
+                roll_score += 1000 * (dice_count - 3)
                 quad_count += 1
     if pair_count == 3:
         roll_score = 1500
     elif triple_count == 2:
         roll_score = 2500
     elif quad_count == 1 and pair_count == 1:
-        roll_score == 1500
+        roll_score = 1500
+    elif check_for_straight(kept_dice_to_check):
+        roll_score = 1500
 
-    print(dice_to_check_counter)
-    print(sorted(kept_dice_to_check))
-    print(roll_score)
-    print(pair_count, triple_count, quad_count)
+    return roll_score
+
+
+def check_for_no_score_dice(dice_to_check=(2, 1, 3, 4, 5, 6)):
+
+    dice_to_check_counter = Counter(dice_to_check)
+    non_score_pair_count = 0
+    pair_count = 0
+    quad_count = 0
+
+    for die_number in (2, 3, 4, 6):
+        if dice_to_check_counter[die_number] == 1:
+            if not check_for_straight(dice_to_check):
+                return True
+        if dice_to_check_counter[die_number] == 2:
+            non_score_pair_count += 1
+            pair_count += 1
+        if dice_to_check_counter[die_number] == 4:
+            quad_count += 1
+
+    for die_number in (1, 5):
+        if dice_to_check_counter[die_number] == 2:
+            pair_count += 1
+        if dice_to_check_counter[die_number] == 4:
+            quad_count += 1
+
+    if non_score_pair_count == 1 and quad_count != 1 and pair_count != 3:
+        return True
 
 
 def main_script_for_now():
 
     print('Welcome to Farkle game!')
-    print('Current ')
     user_play_y_n = input('Play (y/n)?: ')
 
     if user_play_y_n == 'y':
         roll_or_hold = input('Roll or hold (r/h)?: ')
         number_of_dice_to_roll = 6
         hand = Hand()
+        round_score = 0
+        roll_number = 0
         while roll_or_hold == 'r':
+            roll_number += 1
             current_roll = Roll(number_of_dice_to_roll)
             print(f'Dice rolled: {current_roll.roll_dice}')
+            max_roll_score = check_for_score(current_roll.roll_dice)
+            print(f'Roll max score: {current_roll.max_roll_score}')
+            if current_roll.max_roll_score == 0:
+                round_score = 0
+                print('Sorry, you Farkle!')
+                break
             dice_to_hold_index = [int(x) for x in input('Index of dice to hold separated by space (0 is first index): ').split()]
             dice_to_hold = [current_roll.roll_dice[x] for x in dice_to_hold_index]
-            hand.add_dice(dice_to_hold)
-            print('*' * 25)
+            while check_for_no_score_dice(dice_to_hold):
+                print('Sorry, you can only keep scoring dice. Try again.')
+                dice_to_hold_index = [int(x) for x in input('Index of dice to hold separated by space (0 is first index): ').split()]
+                dice_to_hold = [current_roll.roll_dice[x] for x in dice_to_hold_index]
+            if check_for_score(dice_to_hold) > 0:
+                hand.add_dice(dice_to_hold)
+                round_score += check_for_score(dice_to_hold)
+            print('*' * 35)
+            print(f'Roll Number: {roll_number} | Round Score: {round_score}')
             print(f'Hand dice: {hand.hand_dice}')
             number_of_dice_to_roll = 6 - len(hand.hand_dice)
-            print(f'Number of dice to roll: {number_of_dice_to_roll}')
-            print('*' * 25)
-            roll_or_hold = input('Roll or hold (r/h)?: ')
-
-    else:
-        check_for_score()
+            if number_of_dice_to_roll != 0:
+                print(f'Number of dice to roll: {number_of_dice_to_roll}')
+                print('*' * 35)
+                roll_or_hold = input('Roll or hold (r/h)?: ')
+            else:
+                print('Congrats, new roll!')
+                number_of_dice_to_roll = 6
+                hand = Hand()
+        print('*' * 35)
+        print(f'Total round score: {round_score}')
 
 
 if __name__ == '__main__':
 
-    check_for_score()
+    main_script_for_now()
